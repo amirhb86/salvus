234f90f4c87f81b0e120361f899d54fa268837a8
Michael Afanasiev <michael.afanasiev@erdw.ethz.ch>
2016-02-17 10:33:59 +0100

    Began documenting the problem class

--- src/cxx/Problem.h
+++ src/cxx/Problem.h
@@ -1,6 +1,15 @@
-//
-// Created by Michael Afanasiev on 2016-01-27.
-//
+/**
+ * A class to oversee the setup and solution of our problem. Created by Michael Afanasiev on 2016-01-27.
+ * The `Problem` class is responsible for directing the majority of the work done in the program. It should be
+ * inherited from for each conceivable combination of parameters (i.e. there should be a separate derived class for
+ * the scalar wave equation solved on 2-D quads, and the elastic wave equation solved on 2-D quads). What is key, is
+ * that in each derived class, the important variable pointers (mesh, reference element, model) are defined as whatever
+ * type they are required to be for that specific probelm. For instance, in acoustic case, the reference element should
+ * currently be defined as `Acoustic *mReferenceElement`. If this is properly done, an abstract interface can be kept,
+ * which takes a general element and mesh object, with the assumption that within each derived function these objects
+ * will be downcasted (via a <dynamic_cast>).
+ */
+
 
 #ifndef SALVUS_SOLVER_H
 #define SALVUS_SOLVER_H
@@ -42,17 +51,9 @@ private:
 
 public:
 
+
     virtual void solve();
     virtual void initialize(Mesh *mesh, ExodusModel *model, Quad *quad, Options options);
-
-};
-
-class FrequencyDomain : public Problem {
-
-public:
-
-//    virtual void solve();
-//    virtual void initialize(Mesh &mesh, ExodusModel &model, Quad &quad) = 0;
 
 };
 
