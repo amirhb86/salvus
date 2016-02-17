e92194d333dcb6d87c2b658ecf0f4826892c0b59
michael.afanasiev <michael.afanasiev@erdw.ethz.ch>
2016-02-17 19:08:32 +0100

    Calculation of strain for elastic 2d.

--- src/cxx/Element/HyperCube/Quad/Elastic.cpp
+++ src/cxx/Element/HyperCube/Quad/Elastic.cpp
@@ -28,6 +28,39 @@ void Elastic::assembleElementMassMatrix(Mesh *mesh) {
 }
 
 Eigen::MatrixXd Elastic::computeStiffnessTerm(const Eigen::MatrixXd &displacement) {
+
+    int itr = 0;
+    Eigen::Matrix2d inverse_jacobian;
+    Eigen::VectorXd jacobian_determinant(mNumberIntegrationPoints);
+    for (auto eta_index = 0; eta_index < mNumberIntegrationPointsEta; eta_index++) {
+        for (auto eps_index = 0; eps_index < mNumberIntegrationPointsEps; eps_index++) {
+
+            // Eps and eta coordinates.
+            double eta = mIntegrationCoordinatesEta[eta_index];
+            double eps = mIntegrationCoordinatesEps[eps_index];
+
+            jacobian_determinant(itr) = jacobianAtPoint(eps, eta).determinant();
+            inverse_jacobian = jacobianAtPoint(eps, eta).inverse();
+
+            /*
+             * | uxx   uxy  |
+             * |            |
+             * | uyx   uyy  |
+             * Here the strain is calculated for each gll point. At the moment, the full strain matrix is calculated
+             * (see above). It is stored in the pre-allocated matrix containing the strain on each element, in 2x2
+             * blocks. This is why there's the 2*itr+0,1 below.
+             */
+            mElementStrain(0,2*itr+0) = mGradientOperator.row(eps_index).dot(
+                    epsVectorStride(displacement.row(0), eta_index));
+            mElementStrain(0,2*itr+1) = mGradientOperator.row(eta_index).dot(
+                    etaVectorStride(displacement.row(0), eta_index));
+            mElementStrain(1,2*itr+0) = mGradientOperator.row(eps_index).dot(
+                    epsVectorStride(displacement.row(1), eta_index));
+            mElementStrain(1,2*itr+1) = mGradientOperator.row(eta_index).dot(
+                    etaVectorStride(displacement.row(1), eta_index));
+        }
+    }
+
     Eigen::MatrixXd test;
     return test;
 }
