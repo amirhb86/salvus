2fb79aab44f584c82670f65fa39e1521f3ad1804
michael.afanasiev <michael.afanasiev@erdw.ethz.ch>
2016-03-28 13:11:55 +0200

    install petsc with travis

---
+++ conf/install-petsc.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Setup directory tree.
+mkdir -p petsc_install
+mkdir -p petsc_install/lib
+mkdir -p petsc_install/include
+mkdir -p petsc_install/bin
+
+# Linux
+sudo apt-get update -q
+
+# system libs.
+sudo apt-get install build-essential gfortran perl g++ gcc cmake m4 git
+sudo apt-get install -y liblapack-dev liblapack-doc liblapack3gf
+sudo apt-get install -u libblas-dev libblas-doc libblas3gf
+sudo apt-get install -y gfortran openmpi-bin openmpi-common libopenmpi-dev
+
+# PETSc
+git clone -b maint https://bitbucket.org/petsc/petsc petsc; cd petsc
+./configure --download-exodusii --download-netcdf --download-hdf5 --download-chaco
+make PETSC_DIR=/home/travis/petsc PETSC_ARCH=arch-linux2-c-debug all
+make PETSC_DIR=/home/travis/petsc PETSC_ARCH=arch-linux2-c-debug test
+make PETSC_DIR=/home/travis/petsc PETSC_ARCH=arch-linux2-c-debug streams NPMAX=2
